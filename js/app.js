c3d72e3de695665df43ee8d903b4ed79a0b29f32
Hany Luqianie <luqianiehany@gmail.com>
2021-09-18 19:06:50 +0700

    finished the computer rolling choose answer

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection img');
 const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors"];
+const randomComClasses = ["comrock", "compaper","comscissors"];
 const result = document.getElementById('result');
 
 const game = () =>{
@@ -23,82 +24,107 @@ const game = () =>{
 			let clickedBtn = e.target.className;
 			e.target.classList.add("selected");
 
-			let randomNum = Math.floor(Math.random() * randomClasses.length);
-			let computerSelected = randomClasses[randomNum];
-			if(randomNum == 0)
-				document.getElementById("comrock").classList.add("selected");
-			else if(randomNum == 1)
-				document.getElementById("compaper").classList.add("selected");
-			else
-				document.getElementById("comscissors").classList.add("selected");
+			setTimeout(function() {
+				let randomNum = Math.floor(Math.random() * randomComClasses.length);
+				document.getElementById(randomComClasses[randomNum]).classList.add("selected");
+				setTimeout(function() {
+					document.getElementById(randomComClasses[randomNum]).classList.remove("selected");
+					randomNum = Math.floor(Math.random() * randomComClasses.length);
+					setTimeout(function() {
+						document.getElementById(randomComClasses[randomNum]).classList.add("selected");
+						setTimeout(function() {
+							document.getElementById(randomComClasses[randomNum]).classList.remove("selected");
+							randomNum = Math.floor(Math.random() * randomComClasses.length);
+							setTimeout(function() {
+								document.getElementById(randomComClasses[randomNum]).classList.add("selected");
+								setTimeout(function() {
+									document.getElementById(randomComClasses[randomNum]).classList.remove("selected");
+									randomNum = Math.floor(Math.random() * randomComClasses.length);
+									setTimeout(function() {
+										document.getElementById(randomComClasses[randomNum]).classList.add("selected");
+										setTimeout(function() {
+											document.getElementById(randomComClasses[randomNum]).classList.remove("selected");
+											
+											randomNum = Math.floor(Math.random() * randomClasses.length);
+											let computerSelected = randomClasses[randomNum];
+											document.getElementById(randomComClasses[randomNum]).classList.add("selected");
 
-			if(clickedBtn === computerSelected)
-			{
-				pScore.innerHTML = pScore.innerHTML;
-				cScore.innerHTML = cScore.innerHTML;
-				result.innerHTML = "DRAW";
-				result.classList.add("resultdraw");
-				result.classList.remove("resultwin");
-				result.classList.remove("resultlose");
-			} 
-			else if(clickedBtn === randomClasses[0] && computerSelected === randomClasses[2])
-			{
-				pScore.innerHTML = playerScore;
-				playerScore++;
-				result.innerHTML = "PLAYER 1<br>WIN";
-				result.classList.add("resultwin");
-				result.classList.remove("resultdraw");
-				result.classList.remove("resultlose");
-			}
-			else if(clickedBtn === randomClasses[0] && computerSelected === randomClasses[1])
-			{
-				cScore.innerHTML = computerScore;
-				computerScore++;
-				result.innerHTML = "COM<br>WIN";
-				result.classList.add("resultlose");
-				result.classList.remove("resultdraw");
-				result.classList.remove("resultwin");
-			}
-			else if(clickedBtn === randomClasses[1] && computerSelected === randomClasses[2])
-			{
-				cScore.innerHTML = computerScore;
-				computerScore++;
-				result.innerHTML = "COM<br>WIN";
-				result.classList.add("resultlose");
-				result.classList.remove("resultdraw");
-				result.classList.remove("resultwin");
-			}
-			else if(clickedBtn === randomClasses[1] && computerSelected === randomClasses[0])
-			{
-				pScore.innerHTML = playerScore;
-				playerScore++;
-				result.innerHTML = "PLAYER 1<br>WIN";
-				result.classList.add("resultwin");
-				result.classList.remove("resultdraw");
-				result.classList.remove("resultlose");
-			}
-			else if(clickedBtn === randomClasses[2] && computerSelected === randomClasses[0])
-			{
-				cScore.innerHTML = computerScore;
-				computerScore++;
-				result.innerHTML = "COM<br>WIN";
-				result.classList.add("resultlose");
-				result.classList.remove("resultdraw");
-				result.classList.remove("resultwin");
-			}
-			else if(clickedBtn === randomClasses[2] && computerSelected === randomClasses[1])
-			{
-				pScore.innerHTML = playerScore;
-				playerScore++;
-				result.innerHTML = "PLAYER 1<br>WIN";
-				result.classList.add("resultwin");
-				result.classList.remove("resultdraw");
-				result.classList.remove("resultlose");
-			}
-			
-			document.getElementById("rock").style.pointerEvents = "initial";
-			document.getElementById("paper").style.pointerEvents = "initial";
-			document.getElementById("scissors").style.pointerEvents = "initial";
+											if(clickedBtn === computerSelected)
+											{
+												pScore.innerHTML = pScore.innerHTML;
+												cScore.innerHTML = cScore.innerHTML;
+												result.innerHTML = "DRAW";
+												result.classList.add("resultdraw");
+												result.classList.remove("resultwin");
+												result.classList.remove("resultlose");
+											} 
+											else if(clickedBtn === randomClasses[0] && computerSelected === randomClasses[2])
+											{
+												pScore.innerHTML = playerScore;
+												playerScore++;
+												result.innerHTML = "PLAYER 1<br>WIN";
+												result.classList.add("resultwin");
+												result.classList.remove("resultdraw");
+												result.classList.remove("resultlose");
+											}
+											else if(clickedBtn === randomClasses[0] && computerSelected === randomClasses[1])
+											{
+												cScore.innerHTML = computerScore;
+												computerScore++;
+												result.innerHTML = "COM<br>WIN";
+												result.classList.add("resultlose");
+												result.classList.remove("resultdraw");
+												result.classList.remove("resultwin");
+											}
+											else if(clickedBtn === randomClasses[1] && computerSelected === randomClasses[2])
+											{
+												cScore.innerHTML = computerScore;
+												computerScore++;
+												result.innerHTML = "COM<br>WIN";
+												result.classList.add("resultlose");
+												result.classList.remove("resultdraw");
+												result.classList.remove("resultwin");
+											}
+											else if(clickedBtn === randomClasses[1] && computerSelected === randomClasses[0])
+											{
+												pScore.innerHTML = playerScore;
+												playerScore++;
+												result.innerHTML = "PLAYER 1<br>WIN";
+												result.classList.add("resultwin");
+												result.classList.remove("resultdraw");
+												result.classList.remove("resultlose");
+											}
+											else if(clickedBtn === randomClasses[2] && computerSelected === randomClasses[0])
+											{
+												cScore.innerHTML = computerScore;
+												computerScore++;
+												result.innerHTML = "COM<br>WIN";
+												result.classList.add("resultlose");
+												result.classList.remove("resultdraw");
+												result.classList.remove("resultwin");
+											}
+											else if(clickedBtn === randomClasses[2] && computerSelected === randomClasses[1])
+											{
+												pScore.innerHTML = playerScore;
+												playerScore++;
+												result.innerHTML = "PLAYER 1<br>WIN";
+												result.classList.add("resultwin");
+												result.classList.remove("resultdraw");
+												result.classList.remove("resultlose");
+											}
+											
+											document.getElementById("rock").style.pointerEvents = "initial";
+											document.getElementById("paper").style.pointerEvents = "initial";
+											document.getElementById("scissors").style.pointerEvents = "initial";
+											
+										}, 50);
+									}, 50);
+								}, 50);
+							}, 50);
+						}, 50);
+					}, 50);
+				}, 50);
+			}, 50);
         });
     });
 }
